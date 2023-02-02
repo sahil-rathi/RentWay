9b51a2d086461c3d96347077ad0300df74f71101
sahil-rathi <lcs2020018@iiitl.ac.in>
2023-02-02 23:25:55 +0530

    Initial Push
    npx create-next-app
      npm install @mui/material @emotion/react @emotion/styled @emotion/server

---
+++ rentway/pages/index.tsx
@@ -0,0 +1,13 @@
+import react  from "react";
+import { Button } from "@mui/material";
+
+export default function Home() {
+  return (
+    <>
+    Hello
+    <Button>
+      Cart
+    </Button>
+    </>
+  )
+}
